782af4fda9854e0a3943980ccd1de91c643bbe9b
wijinlau <wi6@me.com>
2019-10-31 18:34:24 +0800

    resetup rspec

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_31_023409) do
+ActiveRecord::Schema.define(version: 2019_10_31_100927) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
     t.string "name"
-    t.integer "priority", default: 1, null: false
-    t.integer "status", default: 1, null: false
+    t.integer "priority", default: 1
+    t.integer "status", default: 1
     t.datetime "begin_at"
     t.datetime "end_at"
     t.datetime "created_at", precision: 6, null: false
