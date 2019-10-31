c5b190ff735cfc305fc7913b9bacff03fece9439
wijinlau <wi6@me.com>
2019-10-31 17:43:49 +0800

    generated task model

--- README.md
+++ README.md
@@ -9,9 +9,9 @@
 * id:integer
 * name:string
 * priority:integer
-* status:string
-* begintime:datetime
-* endtime:datetime
+* status:integer
+* begin_at_:datetime
+* end_at_:datetime
 * use_id:integer
 ### task_tag
 * Id:integer
---
+++ db/migrate/20191031023409_create_tasks.rb
@@ -0,0 +1,13 @@
+class CreateTasks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :tasks do |t|
+      t.string :name
+      t.integer :priority, default: 1, null: false 
+      t.integer :status, default: 1, null: false 
+      t.datetime :begin_at
+      t.datetime :end_at
+
+      t.timestamps
+    end
+  end
+end
