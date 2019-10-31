74388d30387ef25f57e4f068fcd0272cb5956cc5
wijinlau <wi6@me.com>
2019-10-31 18:01:33 +0800

    setup spec

---
+++ app/controllers/task_controller.rb
@@ -0,0 +1,8 @@
+class TaskController < ApplicationController
+
+  def index
+    
+    
+  end
+  
+end
---
+++ test/controllers/task_controller_test.rb
@@ -0,0 +1,5 @@
+require 'test_helper'
+
+class TaskControllerTest < ActionDispatch::IntegrationTest
+  
+end
